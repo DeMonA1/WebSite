3dad6256e444b170677ca179011c835ffbb1f3f4
Demon <you@awe.com>
2024-08-15 16:42:04 +0300

    '''

---
+++ sql/zoo_app/login.php
@@ -0,0 +1,13 @@
+<?php // login.php
+    $host = 'localhost';
+    $data = 'publications';
+    $user = 'root';
+    $pass = '{double}1';
+    $chrs = 'utf8mb4';
+    $attr = "mysql:host=$host;dbname=$data;charset=$chrs";
+    $opts = 
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
